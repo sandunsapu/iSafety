e098a5307a36451260ec988daac36e881abb668b
Sandun <snwimalasurendra@gmail.com>
2021-09-12 22:18:28 +0530

    iSafetyBegin

--- app/src/main/java/com/example/isafety/MainActivity.java
+++ app/src/main/java/com/example/isafety/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.isafety;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -11,20 +12,24 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
+    private Button patientBtn,doctorBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
+        //BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
+        //AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+          //      R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+            //    .build();
+        //NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        //NavigationUI.setupWithNavController(navView, navController);
+
+        patientBtn = (Button) findViewById(R.id.patientbtn);
+        doctorBtn=(Button) findViewById(R.id.doctorbtn);
     }
 
 }
