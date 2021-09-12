99ec78a9c79cf16c3bdf92558a9971e3b2cc544a
Sandun <snwimalasurendra@gmail.com>
2021-09-13 03:14:10 +0530

    iSafetyBegin

--- app/src/main/java/com/example/isafety/MainActivity.java
+++ app/src/main/java/com/example/isafety/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.isafety;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -30,6 +32,14 @@ public class MainActivity extends AppCompatActivity {
 
         patientBtn = (Button) findViewById(R.id.patientbtn);
         doctorBtn=(Button) findViewById(R.id.doctorbtn);
+
+        patientBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, patientLoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
---
+++ app/src/main/java/com/example/isafety/patientLoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.isafety;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class patientLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_login);
+    }
+}
