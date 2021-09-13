bf3eb19be2ad98534f77b4408f3273feb0dd50e1
Sandun <snwimalasurendra@gmail.com>
2021-09-14 02:08:07 +0530

    iSafetyBeginRegActivity

---
+++ app/src/main/java/com/example/isafety/PatientRegister4Activity.java
@@ -0,0 +1,14 @@
+package com.example.isafety;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PatientRegister4Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_register4);
+    }
+}
