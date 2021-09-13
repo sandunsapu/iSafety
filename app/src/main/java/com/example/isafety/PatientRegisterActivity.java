961d80aa1d9a8448de3498bb06ad3a5719825662
Sandun <snwimalasurendra@gmail.com>
2021-09-13 22:31:40 +0530

    iSafetyBeginRegActivity

--- app/src/main/java/com/example/isafety/PatientRegisterActivity.java
+++ app/src/main/java/com/example/isafety/PatientRegisterActivity.java
@@ -1,7 +1,10 @@
 package com.example.isafety;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -10,10 +13,27 @@ public class PatientRegisterActivity extends AppCompatActivity {
     private EditText inputNameWithInitials,inputFullName,inputAddress,inputBirthday,inputEmail,inputMobileNumber,inputPasswordRegister,inputPasswordConfirm;
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_register);
+
+        nextRegisterbtn=(Button) findViewById(R.id.register_next_btn);
+        inputNameWithInitials=(EditText) findViewById(R.id.name_with_initials);
+        inputFullName=(EditText) findViewById(R.id.full_name);
+        inputAddress=(EditText) findViewById(R.id.address);
+        inputBirthday=(EditText) findViewById(R.id.birthday);
+        inputEmail=(EditText) findViewById(R.id.register_email_input);
+        inputMobileNumber=(EditText) findViewById(R.id.register_phone);
+        inputPasswordRegister=(EditText) findViewById(R.id.register_password);
+        inputPasswordConfirm=(EditText) findViewById(R.id.register_password_confirm);
+
+        nextRegisterbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PatientRegisterActivity.this,PatientRegister2Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/isafety/patientLoginActivity.java
+++ app/src/main/java/com/example/isafety/patientLoginActivity.java
@@ -2,13 +2,33 @@ package com.example.isafety;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class patientLoginActivity extends AppCompatActivity {
+    private Button loginButton,signInButton;
+    private EditText inputEmail,inputPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_login);
+
+        loginButton=(Button) findViewById(R.id.login_button);
+        signInButton=(Button) findViewById(R.id.sign_in_button);
+        inputEmail=(EditText) findViewById(R.id.login_email_input);
+        inputPassword=(EditText) findViewById(R.id.login_password_input);
+
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(patientLoginActivity.this,PatientRegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
