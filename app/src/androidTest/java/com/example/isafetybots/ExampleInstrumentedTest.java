0309a9479ad793615623994fab2739d8afd46c1a
Sandun <snwimalasurendra@gmail.com>
2021-09-17 19:00:25 +0530

    DashboardDone123aul

--- app/src/androidTest/java/com/example/isafetybots/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/isafetybots/ExampleInstrumentedTest.java
@@ -3,12 +3,12 @@ package com.example.isafetybots;
 import android.content.Context;
 
 import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.isafety", appContext.getPackageName());
+        assertEquals("com.example.isafetybots", appContext.getPackageName());
     }
 }
