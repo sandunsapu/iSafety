261b0cd3a5fcb43a2afd0010c75f918fd179ee9d
Sandun <snwimalasurendra@gmail.com>
2021-09-24 02:38:47 +0530

    Doctor

---
+++ app/src/main/java/com/example/isafetybots/DoctorPatientListActivity.java
@@ -0,0 +1,14 @@
+package com.example.isafetybots;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DoctorPatientListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_patient_list);
+    }
+}
---
+++ app/src/main/java/com/example/isafetybots/DoctorSendInstructionsActivity.java
@@ -0,0 +1,14 @@
+package com.example.isafetybots;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DoctorSendInstructionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_send_instructions);
+    }
+}
