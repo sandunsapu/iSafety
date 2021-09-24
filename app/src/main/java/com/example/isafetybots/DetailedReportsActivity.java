b0fedea56c4d61dd7e24e4226b0aff8dec2ebbbe
Sandun <snwimalasurendra@gmail.com>
2021-09-24 15:53:48 +0530

    Doctor by Sandun

---
+++ app/src/main/java/com/example/isafetybots/DetailedReportsActivity.java
@@ -0,0 +1,33 @@
+package com.example.isafetybots;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+public class DetailedReportsActivity extends AppCompatActivity {
+    private GraphView graphView,graphView2,graphView3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailed_reports);
+
+        graphView = (GraphView) findViewById(R.id.graphView);
+        graphView2 = (GraphView) findViewById(R.id.graphView2);
+        graphView3 = (GraphView) findViewById(R.id.graphView3);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graphView.addSeries(series);
+        graphView2.addSeries(series);
+        graphView3.addSeries(series);
+    }
+}
